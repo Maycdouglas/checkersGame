cdeaf542dc9922b4d4c7ec96873291fd665060de
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2025-06-22 12:07:19 -0300

    Cria menu inicial do jogo

---
+++ Main.hs
@@ -0,0 +1,45 @@
+module Main where
+
+-- Menu inicial do jogo
+main:: IO()
+main = do
+    putStrLn "====================="
+    putStrLn "    JOGO DE DAMAS    "
+    putStrLn "====================="
+    putStrLn "Escoha o modo de jogo:"
+    putStrLn "1 - Jogador vs Máquina"
+    putStrLn "2 - Máquina vs Máquina"
+    putStrLn "3 - Sair"
+    putStrLn "Opção: "
+    option <- getLine
+    case option of
+        "1" -> escolherInicio True
+        "2" -> escolherInicio False
+        "3" -> putStrLn "Saindo do jogo..."
+        _   -> do 
+            putStrLn "Opção inválida, tente novamente."
+            main
+
+-- Função para escolher quem começa o jogo
+escolherInicio :: Bool -> IO ()
+escolherInicio jogadorVsMaquina = do
+    putStrLn "Quem começa o Jogo?"
+    putStrLn "1 - Jogador (ou Máquina 1)"
+    putStrLn "2 - Máquina (ou máquina 2)"
+    putStr "Opção: "
+    inicio <- getLine
+    case inicio of
+        "1" -> iniciarJogo jogadorVsMaquina True
+        "2" -> iniciarJogo jogadorVsMaquina False
+        _   -> do
+            putStrLn "Opção inválida, tente novamente."
+            escolherInicio jogadorVsMaquina
+
+-- Função que inicializa o jogo
+iniciarJogo :: Bool -> Bool -> IO ()
+iniciarJogo jogadorVsMaquina jogadorComeca = do
+    putStrLn $ "\nModo de jogo: " ++
+        if jogadorVsMaquina then "Jogador vs Máquina" else "Máquina vs Máquina"
+    putStrLn $ "Quem começa: " ++ 
+        if jogadorComeca then "Jogador (ou Máquina 1)" else "Máquina (ou Máquina 2)"
+    putStrLn "\nTabuleiro será exibido aqui..."
