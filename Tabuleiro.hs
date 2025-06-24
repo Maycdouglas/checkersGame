4402cf391704874af5ee74169d821ef074669115
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2025-06-24 17:22:43 -0300

    Refatora a exibição do tabuleiro e insere outros comentários

--- Tabuleiro.hs
+++ Tabuleiro.hs
@@ -1,52 +1,65 @@
 module Tabuleiro where
 
-import Control.Monad (guard)
+import Control.Monad (guard) -- função guard da biblioteca Control.Monad
+-- usado em condições, permitindo que o fluxo prossiga se o retorno for True e gerando Nothing se for False
+-- usado em funções com muitas validações, como por exemplo a moverPeca e atualizarCasa
 
 data Peca = PecaJogador1 | PecaJogador2 | DamaJogador1 | DamaJogador2
     deriving (Eq, Show)
-
-pecaAdversaria :: Peca -> Peca -> Bool
-pecaAdversaria PecaJogador1  p = p == PecaJogador2 || p == DamaJogador2
-pecaAdversaria DamaJogador1  p = p == PecaJogador2 || p == DamaJogador2
-pecaAdversaria PecaJogador2  p = p == PecaJogador1 || p == DamaJogador1
-pecaAdversaria DamaJogador2  p = p == PecaJogador1 || p == DamaJogador1
-    
+-- usa o Eq para poder comparar se dois jogadores são iguais ou diferentes
+-- usa o Show para poder converter o texto para String
 
 data Casa = Vazia | Ocupada Peca
     deriving (Eq, Show)
+-- usa o Eq para poder comparar se dois jogadores são iguais ou diferentes
+-- usa o Show para poder converter o texto para String
 
 type Linha = [Casa]
 type Tabuleiro = [Linha]
 
+-- Função para checar se a peça que será capturada é uma adversaria ou nao
+ehPecaAdversaria :: Peca -> Peca -> Bool
+ehPecaAdversaria PecaJogador1  p = p == PecaJogador2 || p == DamaJogador2
+ehPecaAdversaria DamaJogador1  p = p == PecaJogador2 || p == DamaJogador2
+ehPecaAdversaria PecaJogador2  p = p == PecaJogador1 || p == DamaJogador1
+ehPecaAdversaria DamaJogador2  p = p == PecaJogador1 || p == DamaJogador1
+
+-- Função que gera o tabuleiro inicial da partida
 tabuleiroInicial :: Tabuleiro
 tabuleiroInicial =
-    [ linhaMaquina i | i <- [0..2] ] ++
-    replicate 2 linhaVazia ++
-    [ linhaJogador i | i <- [0..2] ]
+    [ linhaJogador2 i | i <- [0..2] ] ++ -- posiciona as peças do jogador 2
+    replicate 2 linhaVazia ++ -- deixa as duas linhas do meio vazias
+    [ linhaJogador1 i | i <- [0..2] ] -- posiciona as peças do jogador 1
   where
-    linhaMaquina i = [if (i + j) `mod` 2 == 1 then Ocupada PecaJogador2 else Vazia | j <- [0..7]]
-    linhaJogador i = [if (i + j) `mod` 2 == 0 then Ocupada PecaJogador1 else Vazia | j <- [0..7]]
+    linhaJogador2 i = [if (i + j) `mod` 2 == 1 then Ocupada PecaJogador2 else Vazia | j <- [0..7]]
+    linhaJogador1 i = [if (i + j) `mod` 2 == 0 then Ocupada PecaJogador1 else Vazia | j <- [0..7]]
     linhaVazia = replicate 8 Vazia
 
+-- Função que exibe o tabuleiro no terminal
 mostrarTabuleiro :: Tabuleiro -> IO ()
 mostrarTabuleiro tab = do
-    putStrLn "   ╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗"
-    mapM_ putStrLn $ zipWith mostrarLinha [8,7..1] tab
-    putStrLn "   ╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝"
-    putStrLn "     A   B   C   D   E   F   G   H"
-  where
-    mostrarLinha i linha =
-        " " ++ show i ++ " ║" ++ concatMap (mostrarCasa i) (zip [0..] linha)
+    putStrLn "   ╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗" -- Desenha a borda superior do tabuleiro
+    mapM_ putStrLn $ zipWith mostrarLinha [8,7..1] tab -- percorre as linhas do tabuleiro mostrando as linhas e colocando a numeracao de cada uma
+    putStrLn "   ╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝" -- Desenha a borda inferior do tabuleiro
+    putStrLn "     A   B   C   D   E   F   G   H" -- Escreve os nomes das colunas no canto inferior
 
-    mostrarCasa linhaIndex (colIndex, casa) =
-        let bg = if (linhaIndex + colIndex) `mod` 2 == 0 then bgBranco else bgPreto
-            texto = case casa of
-                        Vazia -> "   "
-                        Ocupada PecaJogador1   -> corTexto "\x1b[34;1m" " o "
-                        Ocupada PecaJogador2   -> corTexto "\x1b[33;1m" " o "
-                        Ocupada DamaJogador1   -> corTexto "\x1b[34;1m" " D "
-                        Ocupada DamaJogador2   -> corTexto "\x1b[33;1m" " D "
-        in bg texto ++ "║"
+-- Função auxiliar para mostrar a linha do tabuleiro
+mostrarLinha :: Int -> Linha -> String
+mostrarLinha numeroLinha linha =
+    " " ++ show numeroLinha ++ " ║" ++ concatMap (mostrarCasa numeroLinha) (zip [0..] linha) -- aqui tem aplicação parcial. ainda não entendi isso direito
+    -- Pegue a função mostrarCasa com o primeiro argumento fixado (linhaIndex) e aplique ela a cada elemento da lista gerada por zip [0..] linha.
+
+-- Função auxliar para mostrar a casa do tabuleiro
+mostrarCasa :: Int -> (Int, Casa) -> String
+mostrarCasa linhaIndex (colIndex, casa) =
+    let bg = if (linhaIndex + colIndex) `mod` 2 == 0 then bgBranco else bgPreto
+        texto = case casa of
+                    Vazia -> "   "
+                    Ocupada PecaJogador1   -> corTexto "\x1b[34;1m" " o "
+                    Ocupada PecaJogador2   -> corTexto "\x1b[33;1m" " o "
+                    Ocupada DamaJogador1   -> corTexto "\x1b[34;1m" " D "
+                    Ocupada DamaJogador2   -> corTexto "\x1b[33;1m" " D "
+    in bg texto ++ "║"
 
 -- Cores e plano de fundo
 corTexto :: String -> String -> String
@@ -181,7 +194,7 @@ movimentoCapturaValido tab origem destino =
                         abs deltaLinha == 2 && abs deltaColuna == 2 &&
                         casaDestino == Just Vazia &&
                         case casaMeio of
-                            Just (Ocupada pecaMeio) -> pecaAdversaria pecaOrigem pecaMeio
+                            Just (Ocupada pecaMeio) -> ehPecaAdversaria pecaOrigem pecaMeio
                             _ -> False
                     _ -> False
         _ -> False
