dffdb3cd81aaddf168d22d528612e61d4d15cbed
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2025-06-28 12:08:12 -0300

    Torna variaveis mais descritivas em Posicao.hs

--- Posicao.hs
+++ Posicao.hs
@@ -1,18 +1,18 @@
 module Posicao where
 
-import Data.Char (toUpper) -- importa função toUpper do módulo Data.Char para converter letra minúscula em letra maiúscula | Usado em lerPosicao
-import Text.Read (readMaybe) -- importa função readMaybe para tentar converter uma string par anúmero de forma segura | usado no lerPosicao
+import Data.Char (toUpper) -- Importa função toUpper do módulo Data.Char para converter letra minúscula em letra maiúscula | Usado em lerPosicao
+import Text.Read (readMaybe) -- Importa função readMaybe para tentar converter uma string para número de forma segura | usado no lerPosicao
 
 -- Converte coluna de Char para índice Int (A=0, B=1, ..., H=7)
 colunaParaIndice :: Char -> Maybe Int
-colunaParaIndice c
-    | c >= 'A' && c <= 'H' = Just (fromEnum c - fromEnum 'A')
+colunaParaIndice coluna
+    | coluna >= 'A' && coluna <= 'H' = Just (fromEnum coluna - fromEnum 'A')
     | otherwise = Nothing
 
 -- Converte linha (1..8) para índice interno (0..7) invertendo o eixo vertical
 linhaParaIndice :: Int -> Maybe Int
-linhaParaIndice n
-    | n >= 1 && n <= 8 = Just (8 - n)
+linhaParaIndice linha
+    | linha >= 1 && linha <= 8 = Just (8 - linha)
     | otherwise = Nothing
 
 -- Função para verificar se a posição está dentro do tabuleiro e se é uma casa preta
@@ -20,7 +20,7 @@ ehPosicaoValida :: (Int, Int) -> Bool
 ehPosicaoValida (linha, coluna) =
     linha >= 0 && linha < 8 &&
     coluna >= 0 && coluna < 8 &&
-    ((linha + coluna) `mod` 2 == 1)  -- somente casas pretas
+    odd (linha + coluna) -- Somente casas pretas
 
 -- Valida posição dada em formato de interface (linha, coluna) 
 ehPosicaoValidaInterface :: (Int, Char) -> Bool
@@ -29,9 +29,9 @@ ehPosicaoValidaInterface (l, c) = case (linhaParaIndice l, colunaParaIndice c) o
     _ -> False
 
 -- Converte entrada "6 B" para posição (Int, Char), ou seja, (6, 'B')
--- usa o readMaybe para não dar erro em tempo de execução no caso do usuário tentar uma entrada como XB, por exemplo
+-- Usa o readMaybe para não dar erro em tempo de execução no caso do usuário tentar uma entrada como XB, por exemplo
 lerPosicao :: String -> Maybe (Int, Char)
-lerPosicao [l, c]
-  | c >= 'a' && c <= 'h' = (,) <$> readMaybe [l] <*> Just (toUpper c)
-  | c >= 'A' && c <= 'H' = (,) <$> readMaybe [l] <*> Just c
+lerPosicao [linha, coluna]
+  | coluna >= 'a' && coluna <= 'h' = (,) <$> readMaybe [linha] <*> Just (toUpper coluna)
+  | coluna >= 'A' && coluna <= 'H' = (,) <$> readMaybe [linha] <*> Just coluna
 lerPosicao _ = Nothing
